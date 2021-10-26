5ab0a180ea4e18bdbb86caf18e2c05152c38a647
Mark Eli <medo963sy@gmail.com>
2021-10-25 17:14:15 -0700

    Predict Destructuring

---
+++ JavaScript/PredictDestructuring.js
@@ -0,0 +1,71 @@
+//Problem 1
+
+const cars = ['Tesla', 'Mercedes', 'Honda']
+const [ randomCar ] = cars
+const [ ,otherRandomCar ] = cars
+//Predict the output
+console.log(randomCar)
+console.log(otherRandomCar)
+
+//My guess: Tesla, Mercedes
+// output after running: Tesla, Mercedes
+
+//Problem 2
+
+const employee = {
+    name: 'Elon',
+    age: 47,
+    company: 'Tesla'
+}
+const { name: otherName } = employee;
+//Predict the output
+console.log(name);
+console.log(otherName);
+//My guess: undefined, undefined
+// output after running: name is not defined, second console log does not run
+
+// Problem 3
+
+const person = {
+    name: 'Phil Smith',
+    age: 47,
+    height: '6 feet'
+}
+const password = '12345';
+const { password: hashedPassword } = person;  
+//Predict the output
+console.log(password);
+console.log(hashedPassword);
+
+//My guess: 12345, undefined
+// output after running: 12345, undefined
+
+//Problem 4 
+
+const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
+const [,first] = numbers;
+const [,,,second] = numbers;
+const [,,,,,,,,third] = numbers;
+//Predict the output
+console.log(first == second);
+console.log(first == third);
+//My guess: false, true
+// output after running: false, true
+
+//Problem 5
+
+const lastTest = {
+    key: 'value',
+    secondKey: [1, 5, 1, 8, 3, 3]
+}
+const { key } = lastTest;
+const { secondKey } = lastTest;
+const [ ,willThisWork] = secondKey;
+//Predict the output
+console.log(key);
+console.log(secondKey);
+console.log(secondKey[0]);
+console.log(willThisWork);
+
+//My guess: value, [1, 5, 1, 8, 3, 3] , 1 ,5 
+// output after running: value, [1, 5, 1, 8, 3, 3] , 1 ,5 
