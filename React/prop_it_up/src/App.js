adf59172278ec877cde9090c3e28e75ae096fee2
Mark Eli <medo963sy@gmail.com>
2021-10-27 20:11:53 -0700

    Putting it Together

--- React/prop_it_up/src/App.js
+++ React/prop_it_up/src/App.js
@@ -3,7 +3,7 @@ import PropIt from './components/PropIt';
 function App() {
   return (
     <div className="App">
-      <PropIt firstName ={"Jane"} lastName ={"Doe"} age = {45} hairColor ={"Black"} />
+      <PropIt firstName ={"Jane"} lastName ={"Doe"} age = {30} hairColor ={"Black"} />
       <PropIt firstName ={"John"} lastName ={"Smoth"} age = {88} hairColor ={"Brown"} />
       <PropIt firstName ={"Fillmore"} lastName ={"Millard"} age = {50} hairColor ={"Brown"} />
       <PropIt firstName ={"Maria"} lastName ={"Smith"} age = {62} hairColor ={"Brown"} />
--- React/prop_it_up/src/components/PropIt.jsx
+++ React/prop_it_up/src/components/PropIt.jsx
@@ -1,13 +1,24 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
-class PropIt extends Component{
-
-    render(){
+class PropIt extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            bd: this.props.age
+        };
+    }
+    bDay = () => {
+        this.setState({
+            bd: this.state.bd + 1
+        })
+    }
+    render() {
         return (
             <>
-            <h1>{this.props.lastName}, {this.props.firstName}</h1>
-            <p>Age: {this.props.age}</p>
-            <p>Hair color: {this.props.hairColor}</p>
+                <h1>{this.props.lastName}, {this.props.firstName}</h1>
+                <p>Age: {this.state.bd}</p>
+                <p>Hair color: {this.props.hairColor}</p>
+                <button onClick={this.bDay}>Birthday!!</button>
             </>
         );
     }
