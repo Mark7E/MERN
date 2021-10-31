b43fef9732895fcf498afe7b8b6579e10e731b45
Mark Eli <medo963sy@gmail.com>
2021-10-31 13:35:41 -0700

    more forms

--- React/hook_form/src/components/HookForm.jsx
+++ React/hook_form/src/components/HookForm.jsx
@@ -7,42 +7,99 @@ const HookForm = (props) => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("");
+    const [person, setPerson] = useState({ firstName, lastName, email, password, confirmPassword })
+    const [firstNameError, setFirstNameError] = useState("");
+    const [lastNameError, setLastNameError] = useState("");
+    const [emailError, setEmailError] = useState("");
+    const [passwordError, setPasswordError] = useState("");
+    const [confrimPasswordError, setConfrimPasswordError] = useState("");
 
-    
+    // const error = (e) => {
+    //     setError(e.target.value);
+    //     if(e.target.value.length < 2) {
+    //         setError("Name is required!");
+    //     }
+    // }
+
+
     const createUser = (e) => {
         e.preventDefault();
-        const newUser = { firstName, lastName, email, password, confirmPassword };
+        if (firstName.length < 2 && firstName.length > 0){
+            setFirstNameError("First name must be at least 2 charachters")
+        }
+        if (lastName.length < 2 && lastName.length > 0 ){
+            setLastNameError("Last name must be at least 2 charachters")
+        }
+        if(email.length < 5 && email.length > 0){
+            setEmailError("Email must be at least 5 characters.")
+        }
+        if(password.length <= 8 && password.length > 0){
+            setPasswordError("Password must be at least 8 characters!")
+        }
+        // if( password.length == 0 && confirmPassword.length == 0){
+        //     setPasswordError("Please enter a secure password!")
+        // }
+        if(password > 8 && password !== confirmPassword){
+            setConfrimPasswordError("Passwords don't match!")
+        }
+        
 
+
+        const newUser = { firstName, lastName, email, password, confirmPassword };
+        setPerson(newUser)
 
         console.log("Welcome", newUser);
     };
-    
+
 
     return (
         <div>
             <form onSubmit={createUser}>
+                    {
+                        
+                        firstNameError ?  <p> {firstNameError} </p>
+                            : ""
+                    }
                 <div>
                     <label>First Name: </label>
                     <input type="text" onChange={(e) => setFirstName(e.target.value)} value={firstName} />
                 </div>
+                {
+                        
+                        lastNameError? <p> {lastNameError} </p>
+                            : ""
+                    }
                 <div>
                     <label>Last Name: </label>
                     <input type="text" onChange={(e) => setLastName(e.target.value)} value={lastName} />
                 </div>
+                {
+                    emailError? <p> {emailError} </p>
+                    : ""
+                }
                 <div>
                     <label>Email Address: </label>
-                    <input type="text" onChange={(e) => setEmail(e.target.value)} value={email}/>
+                    <input type="text" onChange={(e) => setEmail(e.target.value)} value={email} />
                 </div>
+                {
+                    passwordError ? <p> {passwordError} </p>
+                    : ""
+                }
                 <div>
                     <label>Password: </label>
-                    <input type="text" onChange={(e) => setPassword(e.target.value)} value={password}/>
+                    <input type="text" onChange={(e) => setPassword(e.target.value)} value={password} />
                 </div>
+                {
+                    confrimPasswordError ? <p> {confrimPasswordError} </p>
+                    : ""
+                }
                 <div>
-                    <label>Password: </label>
-                    <input type="text" onChange={(e) => setConfirmPassword(e.target.value)} value={confirmPassword}/>
+                    <label>Confirm Password: </label>
+                    <input type="text" onChange={(e) => setConfirmPassword(e.target.value)} value={confirmPassword} />
                 </div>
                 <input type="submit" value="Create User" />
             </form>
+
             <p>First Name: {firstName}</p>
             <p>Last Name: {lastName}</p>
             <p>Email: {email}</p>
---
+++ algos/10-29-2021.js
@@ -0,0 +1,219 @@
+// Array: Merge Sort
+// visualization: https://www.hackerearth.com/practice/algorithms/sorting/merge-sort/visualize/
+// multiple sorts, visualized: https://i.imgur.com/fq0A8hx.gif
+
+// Time Complexity
+//  - Best case: O(n log(n)
+//  - Average case: O(n log(n))
+//   - Worst case: O(n log(n))
+
+// MergeSort(arr[], l,  r)
+// If r > l
+//      1. Find the middle point to divide the array into two halves:
+//              middle m = (l+r)/2
+//      2. Call mergeSort for first half:
+//              Call mergeSort(arr, l, m)
+//      3. Call mergeSort for second half:
+//              Call mergeSort(arr, m+1, r)
+//      4. Merge the two halves sorted in step 2 and 3:
+//              Call mergeSortedArrays(arr1, arr2)
+
+var testArr = [88, 22, 44, 12, 99, 111, 9, 15, 49];
+// [88, 22, 44, 12, 99, 111, 9, 15, 49];
+// [88, 22, 44, 12]     99, 111, 9, 15, 49
+// 88, 22
+// 88
+
+// main recursive function that will be passed in mergeSortedArrays()
+function mergeSort(arr) {}
+
+
+
+// helper function
+// 👉 try solving this first! 
+// return a new sorted array with all of the values of arr1 and arr2
+var mergeArr1 = [33,44];
+var mergeArr2 = [55,66];
+
+var mergeArrA = [33,44,55];
+var mergeArrB = [11,66];
+
+var arrLeft = [22];
+var arrRight = [11,33];
+//                      arrLeft, arrRight
+function mergeSortedArrays(arr1, arr2) {
+    let newArr = []
+    let i =0;
+    while(arr1.length <i || arr2.length < i){
+            if (arr1[i] < arr2[i]){
+                newArr.push(arr1[i])
+            }
+            else if  (arr1[i] >arr2[i]){
+                newArr.push(arr2[i])
+            }            
+            
+            else if  (arr2[i] < arr1[i]){
+                newArr.push(arr2[i])
+            }
+            else if  (arr2[i] > arr1[i]){
+                newArr.push(arr2[i])
+            }
+                
+            // else{
+
+            //     newArr.push(arr1[i], arr2[i])
+            // }
+
+
+            return newArr
+        }
+        i++
+}
+
+
+
+console.log(mergeSortedArrays(mergeArr1, mergeArr2))
+
+// //steps:
+//     1. create a merge function to merge two already sorted arrays into a single sorted array
+//       - you do NOT need to work in place, ok to use a new array
+//     2. create merge sort function to sort the provided array
+//       - split the array into half and recursively merge the halves (run mergeSort on those 2 halves)
+//       - splitting of arrays stops when array can no longer be split
+//       - an array of 1 item is by definition sorted, so two arrays of 1 item can therefore be merged into a sorted array
+
+
+
+//   useful methods
+//   - var x = arr1.concat(array2, array3)
+//     - combines array arg(s) with the array that concat is called on
+//     - returns new arr w/o mutating originals
+
+
+//   - arr.slice([begin[, end]])
+//     - returns a COPY of the specified portion of the array
+//     - end is exclusive
+//       - if omitted, slices from provided 'begin' to end of array
+
+// var arr = [88, 22, 44, 12, 99, 111, 9, 15, 49]
+// var m = Math.floor(arr.length / 2)
+// var l = arr.slice(0, m);
+// var r = arr.slice(m, arr.length);
+
+
+
+
+
+
+
+// Array: Merge Sort
+// visualization: https://www.hackerearth.com/practice/algorithms/sorting/merge-sort/visualize/
+// multiple sorts, visualized: https://i.imgur.com/fq0A8hx.gif
+
+// Time Complexity
+//  - Best case: O(n log(n)
+//  - Average case: O(n log(n))
+//   - Worst case: O(n log(n))
+
+// MergeSort(arr[], l,  r)
+// If r > l
+//      1. Find the middle point to divide the array into two halves:
+//              middle m = (l+r)/2
+//      2. Call mergeSort for first half:
+//              Call mergeSort(arr, l, m)
+//      3. Call mergeSort for second half:
+//              Call mergeSort(arr, m+1, r)
+//      4. Merge the two halves sorted in step 2 and 3:
+//              Call mergeSortedArrays(arr1, arr2)
+
+var testArr = [88, 22, 44, 12, 99, 111, 9, 15, 49];
+// [88, 22, 44, 12, 99, 111, 9, 15, 49];
+// [88, 22, 44, 12]     99, 111, 9, 15, 49
+// 88, 22
+// 88
+
+// main recursive function that will be passed in mergeSortedArrays()
+function mergeSort(arr) {
+    if (arr.length == 1) {
+        return arr
+    }
+
+ let half = Math.floor(arr.length / 2)
+ var l = arr.slice(0, half);
+ var r = arr.slice(half, arr.length);
+
+return mergeSortedArrays(mergeSort(l), mergeSort(r));
+}
+
+console.log(mergeSort(testArr))
+
+
+// **********************************************
+
+
+var mergeArr2 = [55,66];
+var mergeArr1 = [33,44];
+
+var mergeArrA = [33,44,55];
+var mergeArrB = [11,66];
+
+var arrLeft = [22];
+var arrRight = [11,33];
+//                      arrLeft, arrRight
+function mergeSortedArrays(arr1, arr2) {
+    let result = [];
+    let i = 0;
+    let j = 0;
+
+    while(i < arr1.length && j < arr2.length){
+        if(arr1[i] > arr2[j]){
+            result.push(arr2[j]);
+            j++;
+        }else{
+            result.push(arr1[i]);
+            i++;
+        }
+    }
+    while(i < arr1.length){
+        result.push(arr1[i]);
+        i++;
+    }
+
+    while(j < arr2.length){
+        result.push(arr2[j]);
+        j++;
+    }
+    return result;
+}
+
+console.log(mergeSortedArrays(mergeArr2, mergeArr1))
+console.log(mergeSortedArrays(mergeArrA, mergeArrB))
+console.log(mergeSortedArrays(arrRight, arrLeft))
+
+
+
+// //steps:
+//     1. create a merge function to merge two already sorted arrays into a single sorted array
+//       - you do NOT need to work in place, ok to use a new array
+//     2. create merge sort function to sort the provided array
+//       - split the array into half and recursively merge the halves (run mergeSort on those 2 halves)
+//       - splitting of arrays stops when array can no longer be split
+//       - an array of 1 item is by definition sorted, so two arrays of 1 item can therefore be merged into a sorted array
+
+
+
+//   useful methods
+//   - var x = arr1.concat(array2, array3)
+//     - combines array arg(s) with the array that concat is called on
+//     - returns new arr w/o mutating originals
+
+
+//   - arr.slice([begin[, end]])
+//     - returns a COPY of the specified portion of the array
+//     - end is exclusive
+//       - if omitted, slices from provided 'begin' to end of array
+
+// var arr = [88, 22, 44, 12, 99, 111, 9, 15, 49]
+// var m = Math.floor(arr.length / 2)
+// var l = arr.slice(0, m);
+// var r = arr.slice(m, arr.length);
